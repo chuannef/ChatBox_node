30e76f596c1a1de13d22da0a2069c91127c9abc6
djksanrs <chinh99562@donga.edu.vn>
2024-12-12 19:11:15 +0700

    onError and onListing

--- config/configServer.js
+++ config/configServer.js
@@ -16,6 +16,8 @@ export function configServer(app) {
   // });
 
   server.listen(port);
+  server.on('error', onError);
+  server.on('listening', onListening);
 
   function normalizePort(val) {
     const port = parseInt(val, 10);
@@ -30,4 +32,40 @@ export function configServer(app) {
     return false;
   }
 
+  function onError(error) {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+
+    const bind = typeof port === 'string'
+      ? 'Pipe ' + port
+      : 'Port ' + port;
+
+    // handle specific listen errors with friendly messages
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
+
+  /**
+   * Event listener for HTTP server "listening" event.
+   */
+
+  function onListening() {
+    console.log(`Server listing on port: ${port}`);
+    var addr = server.address();
+    var bind = typeof addr === 'string'
+      ? 'pipe ' + addr
+      : 'port ' + addr.port;
+    debug('Listening on ' + bind);
+  }
 }
