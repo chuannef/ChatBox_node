52da4b7b897d8f1e6e8a400471a97e2b191b71fb
djksanrs <chinh99562@donga.edu.vn>
2024-12-13 14:11:31 +0700

    Enhance schema for security

--- models/user.js
+++ models/user.js
@@ -1,8 +1,20 @@
 import mongoose, { Schema  } from "mongoose";
 
 const user = new Schema({
-  username: String, 
-  password: String,
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    min: 4,
+    max: 12,
+    match: /^[a-zA-Z0-9_]+$/
+  }, 
+  password: {
+    type: String,
+    required: true
+  },
   createdAt: { type: Date, default: Date.now }
 });
 
