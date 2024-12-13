ee775351d8e2864e8fdd3d9d07b132027ec8ad79
djksanrs <chinh99562@donga.edu.vn>
2024-12-13 14:11:45 +0700

    Route for user registrations

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 import {Router} from 'express';
 import UsersController from '../controller/users_controller.js';
+import { loginValidation } from '../middleware/login_validation.js';
+import { registerValidation } from '../middleware/register_validation.js';
 
 let router = Router();
 
@@ -10,6 +12,9 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login', UsersController.login);
-router.post('/login', UsersController.loginRequest);
+router.post('/login', loginValidation, UsersController.loginRequest);
+
+router.get('/register', UsersController.register);
+router.post('/register', registerValidation, UsersController.registreRequest);
 
 export default router;
