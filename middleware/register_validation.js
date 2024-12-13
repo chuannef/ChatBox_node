4514e755e9dc5f9c6ea32bc9c60c9c15bef0857b
djksanrs <chinh99562@donga.edu.vn>
2024-12-13 14:11:03 +0700

    Implement form login and form register validations

---
+++ middleware/register_validation.js
@@ -0,0 +1,22 @@
+import { body, validationResult } from 'express-validator';
+
+export const registerValidation = [
+  body('username') 
+  .trim()
+  .notEmpty()
+  .withMessage('username is required')
+  .matches(/^[a-zA-Z0-9_]+$/)
+  .withMessage('username can only contain letters, numbers and underscores')
+  .isLength({ min: 4, max: 12 })
+  .withMessage('username must be between 4 and 12')
+  .escape(),
+
+  body('password')
+  .trim()
+  .notEmpty()
+  .withMessage('Password is required')
+  .isLength({ min: 5 })
+  .withMessage('password must be at least 5 characters long'),
+
+  validationResult
+]
