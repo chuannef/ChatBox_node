eac17010a7fce1a74c996f3ed81e315400705064
djksanrs <chinh99562@donga.edu.vn>
2024-12-13 14:14:42 +0700

    Update: add oldInput

--- controller/users_controller.js
+++ controller/users_controller.js
@@ -38,19 +38,56 @@ class UsersController {
           httpOnly: true, 
           maxAge: 3600000,
           sameSite: 'strict'
-        });
-        // user's information is correct and token generates without error
-        return res.render('index', { username: user.username, user_id: user._id });
-      } catch (e) {
-        // JWT error
-        console.log(e); // Trace the errors 
-        return res.status(500).render('login', { message: 'Internal server error'});
+  /**
+   * User requests login
+   */
+  static async loginRequest(req, res) {
+    try {
+      const { username, password } = req.body;
+      if (!username || !password) {
+        return res.status(400).render('login', { message: 'username and password are required' });
       }
-    } else {
-      // password is incorrect
-      return res.status(401).render('login', { message: 'Incorrect credentials' });
+      const user = await User.findOne({ username });
+      // There is no user in the database
+      if (!user) return res.status(401).render('login', { message: 'User not found' });
+      // Compare plain password and hashed password
+      const decodedPassword = await bcrypt.compare(password, user.password);
+      // If password is correct
+      if (decodedPassword) {
+        const payload = {
+          user_id: user._id,
+          username: user.username
+        };
+        try {
+          // Generate token, return the JSON web token string. Expires in 1 hour
+          const token = jwt.sign(payload, process.env.JWT_SECRET, { 'expiresIn': '1h', algorithm: 'HS256' });
+          res.cookie('jwt', token, {
+            httpOnly: true, 
+            maxAge: 3600000,
+            sameSite: 'strict'
+          });
+          // user's information is correct and token generates without error
+          return res.render('index', { username: user.username, user_id: user._id });
+        } catch (e) {
+          // JWT error
+          console.log(e); // Trace the errors 
+          return res.status(500).render('login', { message: 'Internal server error'});
+        }
+      } else {
+        // password is incorrect
+        return res.status(401).render('login', { 
+          message: 'Incorrect credentials',
+          oldInput: { username: req.body.username }
+        });
+      }
+    } catch (e) {
+      return res.status(500).render('login', { 
+        message: 'An error occured during login',
+        oldInput: { username: req.body.username }
+      });
+
     }
-    
+
   }
 }
 
