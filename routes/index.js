218075eee4d1b4dfcd808b4879f05dae2988c76e
djksanrs <chinh99562@donga.edu.vn>
2024-12-14 22:54:57 +0700

    Test cookie response from server

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,22 @@ let router = Router();
 /* GET home page. */
 router.get('/', verifyLogin, IndexController.index);
 
+router.get('/test-cookie', (req, res) => {
+    const testToken = 'test-token-' + Date.now();
+    
+    res.cookie('testJwt', testToken, {
+        httpOnly: false,
+        secure: false,
+        sameSite: 'lax',
+        path: '/',
+        maxAge: 3600000
+    });
+
+    res.json({ 
+        message: 'Test cookie set',
+        token: testToken,
+        cookies: req.cookies
+    });
+});
+
 export default router;
