c4a50a186675167bf3834c87a532c4a75c7427bf
djksanrs <chinh99562@donga.edu.vn>
2024-12-14 22:57:29 +0700

    Fix: missing import

--- models/channel.js
+++ models/channel.js
@@ -1,3 +1,5 @@
+import mongoose, { Schema  } from "mongoose";
+
 const channelSchema = new Schema({
   name: {
     type: String,
--- models/userChannel.js
+++ models/userChannel.js
@@ -1,11 +1,13 @@
+import mongoose, { Schema  } from "mongoose";
+
 const userChannelSchema = new Schema({
   userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   channelId: { type: Schema.Types.ObjectId, ref: 'Channel', required: true },
-  role: { 
-    type: String, 
-    enum: ['member', 'admin'], 
-    default: 'member' 
-  },
+  // role: { 
+  //   type: String, 
+  //   enum: ['member', 'admin'], 
+  //   default: 'member' 
+  // },
   joinedAt: { type: Date, default: Date.now }
 });
 
