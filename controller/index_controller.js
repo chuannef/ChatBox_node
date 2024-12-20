bffd155ff50873a9984c71268387dbda7f0b3f9a
djksanrs <chinh99562@donga.edu.vn>
2024-12-21 00:22:53 +0700

    Modify how to fetch channels

--- controller/index_controller.js
+++ controller/index_controller.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose';
-import { User } from '../models/user.js';
-import { UserChannel } from '../models/userChannel.js';
-import { Channel } from '../models/channel.js';
-import { Message } from '../models/message.js';
+import {User} from '../models/user.js';
+import {UserChannel} from '../models/userChannel.js';
+import {Channel} from '../models/channel.js';
+import {Message} from '../models/message.js';
 
 class IndexController {
   /** 
@@ -13,24 +13,22 @@ class IndexController {
 
     // find all channels to display in sidebar
     try {
-      // const channels = await Channel.find();
       const controller = new IndexController();
       const channels = await controller.getChannels(user.id);
-      console.log(channels);
 
-      return res.render('index', { username: user.username, 
+      return res.render('index', { 
+        username: user.username, 
         user_id: user.id,
         channels: channels
       });
     } catch (e) {
       console.error(e);
-      return;
+
     }
   }
 
   async getChannels(userId) {
-    const channels = await Channel.aggregate([
-      // First get channels where user is a participant
+    return Channel.aggregate([
       {
         $lookup: {
           from: 'userchannels',
@@ -41,48 +39,19 @@ class IndexController {
                 $expr: {
                   $and: [
                     { $eq: ['$channelId', '$$channelId'] },
-                    { $eq: ['$userId', new mongoose.Types.ObjectId(userId)] }
+                    { $eq: ['$userId', new mongoose.Types.ObjectId(userId)] },
+                    { $eq: ['$status', 'accepted'] }  // Only get accepted channels
                   ]
                 }
               }
             }
           ],
-          as: 'userChannelRelation'
+          as: 'userChannel'
         }
       },
-      // Only include channels where user has a relationship
       {
         $match: {
-          'userChannelRelation.0': { $exists: true }
-        }
-      },
-      // Get channel creator info
-      {
-        $lookup: {
-          from: 'userchannels',
-          let: { channelId: '$_id' },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $and: [
-                    { $eq: ['$channelId', '$$channelId'] },
-                    { $eq: ['$role', 'admin'] } // Admin is the one who created this
-                  ]
-                }
-              }
-            },
-            {
-              $lookup: {
-                from: 'users',
-                localField: 'userId',
-                foreignField: '_id',
-                as: 'creator'
-              }
-            },
-            { $unwind: '$creator' }
-          ],
-          as: 'channelCreator'
+          userChannel: { $ne: [] }  // Channel must have a UserChannel entry
         }
       },
       // Get latest message
@@ -107,6 +76,16 @@ class IndexController {
           as: 'latestMessage'
         }
       },
+
+      // Get creator info
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'creator',
+          foreignField: '_id',
+          as: 'creatorInfo'
+        }
+      },
       // Get message count for last 24 hours
       {
         $lookup: {
@@ -126,66 +105,67 @@ class IndexController {
           as: 'messageCount'
         }
       },
-      // Format the output
+      // Format output
       {
         $addFields: {
           latestMessage: { $arrayElemAt: ['$latestMessage', 0] },
+          userRole: { $arrayElemAt: ['$userChannel.role', 0] },
+          userStatus: { $arrayElemAt: ['$userChannel.status', 0] },
+          creator: {
+            $let: {
+              vars: {
+                creator: { $arrayElemAt: ['$creatorInfo', 0] }
+              },
+              in: {
+                _id: '$$creator._id',
+                username: '$$creator.username'
+              }
+            }
+          },
           messageCount: {
             $ifNull: [
               { $arrayElemAt: ['$messageCount.count', 0] },
               0
             ]
-          },
-          userRole: { $arrayElemAt: ['$userChannelRelation.role', 0] },
-          creator: {
-            $ifNull: [
-              { 
-                $let: {
-                  vars: {
-                    creator: { $arrayElemAt: ['$channelCreator.creator', 0] }
-                  },
-                  in: {
-                    _id: '$$creator._id',
-                    username: '$$creator.username'
-                  }
-                }
-              },
-              null
-            ]
           }
         }
       },
-      // Sort by latest message
+      // Remove unnecessary fields
+      {
+        $project: {
+          userChannel: 0,
+          creatorInfo: 0
+        }
+      },
+      // Sort by latest activity
       {
         $sort: {
+          lastActivity: -1,
           'latestMessage.sentAt': -1
         }
       }
     ]);
-
-    return channels;
   }
 
   async getUsers(currentUserId) {
-    console.log(currentUserId);
     try {
-      const users = await User.aggregate([
+      return await User.aggregate([
         {
           $match: {
-            _id: { $ne: new mongoose.Types.ObjectId(currentUserId) } // Exclude current user
+            _id: {$ne: new mongoose.Types.ObjectId(currentUserId)} // Exclude current user
           }
         },
         {
           $lookup: {
             from: 'conversations',
-            let: { userId: '$_id' },
+            let: {userId: '$_id'},
             pipeline: [
               {
                 $match: {
                   $expr: {
                     $and: [
-                      { $in: ['$$userId', '$participants'] },
-                      { $in: [new mongoose.Types.ObjectId(currentUserId), '$participants'] }
+                      {$in: ['$$userId', '$participants']},
+                      {$in: [new mongoose.Types.ObjectId(currentUserId), '$participants']}
                     ]
                   }
                 }
@@ -204,13 +184,11 @@ class IndexController {
         },
         {
           $addFields: {
-            conversation: { $arrayElemAt: ['$conversation', 0] },
-            lastMessage: { $arrayElemAt: ['$conversation.lastMessage', 0] }
+            conversation: {$arrayElemAt: ['$conversation', 0]},
+            lastMessage: {$arrayElemAt: ['$conversation.lastMessage', 0]}
           }
         }
       ]);
-
-      return users;
     } catch (error) {
       console.error('Error getting users:', error);
       throw error;
